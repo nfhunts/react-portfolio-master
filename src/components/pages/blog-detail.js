77fe073446645d19c20e22871e557d4bae652527
nfhunts <nfhunts@gmail.com>
2020-09-05 10:26:55 -0600

    enable auth rules

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -16,17 +16,37 @@ export default class BlogDetail extends Component {
     };
 
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false
+    });
+  }
+
+  handleFeaturedImageDelete() {
+    this.setState({
+      blogItem: {
+        featured_image_url: ""
+      }
+    });
   }
 
   handleEditClick() {
-    console.log("handle edit clicked");
-    this.setState({ editMode: true });
+    if (this.props.loggedInStatus === "LOGGED_IN") {
+      this.setState({ editMode: true });
+    }
   }
 
   getBlogItem() {
     axios
       .get(
-        `https://jordan.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+        `https://nfhunts.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
       )
       .then(response => {
         this.setState({
@@ -53,7 +73,12 @@ export default class BlogDetail extends Component {
     const contentManager = () => {
       if (this.state.editMode) {
         return (
-          <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />
+          <BlogForm
+            handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+            editMode={this.state.editMode}
+            blog={this.state.blogItem}
+          />
         );
       } else {
         return (
